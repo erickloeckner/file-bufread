379c41f6962ae136a3a86e950e71a1f60b4ce82a
erickloeckner <48753426+erickloeckner@users.noreply.github.com>
2019-12-25 22:32:33 -0500

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::env;
 use std::fs::File;
 use std::io::{self, BufReader};
@@ -7,73 +6,87 @@ use std::path::PathBuf;
 
 const PROJECT: &str = "file-bufread";
 
+struct ReqCount {
+    name: String,
+    count: u32,
+}
+
 fn main() -> io::Result<()> {
     let home_str = env::var("HOME").unwrap();
     let mut home_path = PathBuf::from(home_str);
-    //~ println!("{:?}", home_path);
-    
     home_path.push(PROJECT);
     home_path.push("log.txt");
+    
     let f = File::open(home_path.to_str().unwrap())?;
     let mut f = BufReader::new(f);
     
-    let per_ip = req_per_ip(&mut f);
-    let mut per_ip_vec: Vec<_> = per_ip.iter().collect();
-    per_ip_vec.sort_by(|a, b| a.1.cmp(b.1).reverse());
+    let mut per_ip = req_per_ip(&mut f);
+    per_ip.sort_unstable_by(|a, b| a.count.cmp(&b.count).reverse());
+    
+    let mut per_uri = req_per_uri(&mut f);
+    per_uri.sort_unstable_by(|a, b| a.count.cmp(&b.count).reverse());
     
     println!("IP: # of requests");
-    print_map(&per_ip_vec, 10);
+    print_map(&per_ip, 10);
     println!("-----------------");
-    
-    let per_uri = req_per_uri(&mut f);
-    let mut per_uri_vec: Vec<_> = per_uri.iter().collect();
-    per_uri_vec.sort_by(|a, b| a.1.cmp(b.1).reverse());
     
     println!("URI: # of requests");
-    print_map(&per_uri_vec, 10);
+    print_map(&per_uri, 10);
     println!("-----------------");
-
+    
     Ok(())
 }
 
-fn req_per_ip(buf: &mut BufReader<File>) -> HashMap<String, u32> {
-    let mut out: HashMap<String, u32> = HashMap::new();
+fn req_per_ip(buf: &mut BufReader<File>) -> Vec<ReqCount> {
+    let mut out: Vec<ReqCount> = Vec::new();
     buf.seek(io::SeekFrom::Start(0)).unwrap();
     
     for line in buf.by_ref().lines() {
         let line_c = line.unwrap();
-        let field = line_c.split(' ').nth(0).unwrap();
-        match out.get_mut(field) {
-            Some(v) => *v += 1,
-            None => {
-                out.insert(field.to_string(), 1);
-                ()
+        let field = line_c.split(' ').nth(0);
+        match field {
+            Some(v) => {
+                vec_get(&mut out, v);
             },
+            None => (),
         }
     }
     out
 }
 
-fn req_per_uri(buf: &mut BufReader<File>) -> HashMap<String, u32> {
-    let mut out: HashMap<String, u32> = HashMap::new();
+fn req_per_uri(buf: &mut BufReader<File>) -> Vec<ReqCount> {
+    let mut out: Vec<ReqCount> = Vec::new();
     buf.seek(io::SeekFrom::Start(0)).unwrap();
     
     for line in buf.by_ref().lines() {
         let line_c = line.unwrap();
-        let field = line_c.split(' ').nth(6).unwrap();
-        match out.get_mut(field) {
-            Some(v) => *v += 1,
-            None => {
-                out.insert(field.to_string(), 1);
-                ()
+        let field = line_c.split(' ').nth(6);
+        match field {
+            Some(v) => {
+                vec_get(&mut out, v);
             },
+            None => (),
         }
     }
     out
 }
 
-fn print_map(map: &Vec<(&String, &u32)>, num: usize) {
-    for (key, val) in map.iter().take(num) {
-        println!("{}: {}", key, val);
+fn print_map(map: &Vec<ReqCount>, num: usize) {
+    for i in map.iter().take(num) {
+        println!("{}: {}", i.name, i.count);
+    }
+}
+
+fn vec_get(v: &mut Vec<ReqCount>, s: &str) {
+    let pos_ref = v.iter().enumerate().filter(|i| (i.1).name == s).next();
+    
+    match pos_ref {
+        Some(i) => { 
+            let pos = i.0;
+            v[pos].count += 1;
+        },
+        None => {
+            v.push(ReqCount {name: String::from(s), count: 1});
+        },
     }
 }
